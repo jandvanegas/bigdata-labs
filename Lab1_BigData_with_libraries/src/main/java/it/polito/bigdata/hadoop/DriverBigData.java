31b8c25c83903aae4c000456927b3ed6573a2961
jandvanegas <jandvanegas@gmail.com>
2022-01-25 20:48:51 +0100

    lab(lab2): applied 2grams frequency.

--- Lab1_BigData_with_libraries/src/main/java/it/polito/bigdata/hadoop/DriverBigData.java
+++ Lab1_BigData_with_libraries/src/main/java/it/polito/bigdata/hadoop/DriverBigData.java
@@ -13,85 +13,60 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-/**
- * MapReduce program
- */
+import java.io.IOException;
+
 public class DriverBigData extends Configured implements Tool {
 
-	@Override
-	public int run(String[] args) throws Exception {
+    @Override
+    public int run(String[] args) throws Exception {
 
-		Path inputPath;
-		Path outputDir;
-		int numberOfReducers;
-		int exitCode;
+        Path inputPath;
+        Path outputDir;
+        int numberOfReducers;
+        int exitCode;
 
-		// Parse the parameters
-		// Number of instances of the reducer class
-		numberOfReducers = Integer.parseInt(args[0]);
-		// Folder containing the input data
-		inputPath = new Path(args[1]);
-		// Output folder
-		outputDir = new Path(args[2]);
+        numberOfReducers = Integer.parseInt(args[0]);
+        inputPath = new Path(args[1]);
+        outputDir = new Path(args[2]);
 
-		Configuration conf = this.getConf();
+        Job job = getJob(inputPath, outputDir, numberOfReducers);
 
-		// Define a new job
-		Job job = Job.getInstance(conf);
+        if (job.waitForCompletion(true))
+            exitCode = 0;
+        else
+            exitCode = 1;
 
-		// Assign a name to the job
-		job.setJobName("Basic MapReduce Project - WordCount example");
+        return exitCode;
+    }
 
-		// Set path of the input file/folder (if it is a folder, the job reads all the
-		// files in the specified folder) for this job
-		FileInputFormat.addInputPath(job, inputPath);
+    private Job getJob(Path inputPath, Path outputDir, int numberOfReducers) throws IOException {
+        Configuration conf = this.getConf();
+        Job job = Job.getInstance(conf);
+        job.setJobName("Basic MapReduce Project - WordCount example");
+        //if it is a folder, the job reads all the
+        // files in the specified folder) for this job
+        FileInputFormat.addInputPath(job, inputPath);
+        FileOutputFormat.setOutputPath(job, outputDir);
+        job.setJarByClass(DriverBigData.class);
 
-		// Set path of the output folder for this job
-		FileOutputFormat.setOutputPath(job, outputDir);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
 
-		// Specify the class of the Driver for this job
-		job.setJarByClass(DriverBigData.class);
+        job.setMapperClass(MapperBigData.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
 
-		// Set job input format
-		job.setInputFormatClass(TextInputFormat.class);
+        job.setReducerClass(ReducerBigData.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
 
-		// Set job output format
-		job.setOutputFormatClass(TextOutputFormat.class);
+        job.setNumReduceTasks(numberOfReducers);
+        return job;
+    }
 
-		// Set map class
-		job.setMapperClass(MapperBigData.class);
+    public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new Configuration(), new DriverBigData(), args);
 
-		// Set map output key and value classes
-		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(IntWritable.class);
-
-		// Set reduce class
-		job.setReducerClass(ReducerBigData.class);
-
-		// Set reduce output key and value classes
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
-
-		// Set number of reducers
-		job.setNumReduceTasks(numberOfReducers);
-
-		// Execute the job and wait for completion
-		if (job.waitForCompletion(true) == true)
-			exitCode = 0;
-		else
-			exitCode = 1;
-
-		return exitCode;
-	}
-
-	/**
-	 * Main of the driver
-	 */
-
-	public static void main(String args[]) throws Exception {
-		// Exploit the ToolRunner class to "configure" and run the Hadoop application
-		int res = ToolRunner.run(new Configuration(), new DriverBigData(), args);
-
-		System.exit(res);
-	}
+        System.exit(res);
+    }
 }
--- Lab1_BigData_with_libraries/src/main/java/it/polito/bigdata/hadoop/MapperBigData.java
+++ Lab1_BigData_with_libraries/src/main/java/it/polito/bigdata/hadoop/MapperBigData.java
@@ -1,36 +1,33 @@
 package it.polito.bigdata.hadoop;
 
 import java.io.IOException;
+import java.text.MessageFormat;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-/**
- * Basic MapReduce Project - Mapper
- */
 class MapperBigData extends Mapper<LongWritable, // Input key type
-		Text, // Input value type
-		Text, // Output key type
-		IntWritable> {// Output value type
+        Text, // Input value type
+        Text, // Output key type
+        IntWritable> {// Output value type
 
-	protected void map(LongWritable key, // Input key type
-			Text value, // Input value type
-			Context context) throws IOException, InterruptedException {
+    protected void map(LongWritable key, // Input key type
+                       Text value, // Input value type
+                       Context context) throws IOException, InterruptedException {
 
-		// Split each sentence in words. Use whitespace(s) as delimiter
-		// (=a space, a tab, a line break, or a form feed)
-		// The split method returns an array of strings
-		String[] words = value.toString().split("\\s+");
-
-		// Iterate over the set of words
-		for (String word : words) {
-			// Transform word case
-			String cleanedWord = word.toLowerCase();
-
-			// emit the pair (word, 1)
-			context.write(new Text(cleanedWord), new IntWritable(1));
-		}
-	}
+        String[] words = value.toString().split("\\s+");
+        String previousWord = null;
+        for (String word : words) {
+            String currentWord = word.toLowerCase();
+            if (previousWord == null) {
+                previousWord = currentWord;
+                continue;
+            }
+            context.write(new Text(MessageFormat.format("{0} {1}", previousWord, currentWord)),
+                    new IntWritable(1));
+            previousWord = currentWord;
+        }
+    }
 }
